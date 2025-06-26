83ebdef407e2d7ca6c7588228e9e53ef140ec01a
ZynnHub <isaackee798@gmail.com>
2025-06-26 18:21:33 +0800

    Update Skid%20Hub%20X.lua

--- Skid%20Hub%20X.lua
+++ Skid%20Hub%20X.lua
@@ -26,7 +26,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true,
-    Theme = "Dark",
+    Theme = "Darker",
     MinimizeKey = Enum.KeyCode.LeftControl
 })
 
